c352c47d1493ec77dba619d52e9b9ccb894ca575
Phystro <phystroq@gmail.com>
2023-04-24 21:48:03 +0300

    fix: Mappers and business logic in application

--- src/Core/Application/_Import.cs
+++ src/Core/Application/_Import.cs
@@ -1,8 +1,10 @@
+global using AutoMapper;
 global using MediatR;
 global using System.Reflection;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.DependencyInjection;
 
 global using Kasi.Domain.Entities;
+global using Kasi.Application.Interfaces;
 global using Kasi.Domain.DTO.DriverDTO;
 global using Kasi.Domain.DTO.TeamDTO;
--- src/Core/Domain/Entities/Team.cs
+++ src/Core/Domain/Entities/Team.cs
@@ -5,6 +5,6 @@ namespace Kasi.Domain.Entities
         public string Name { get; set; } = string.Empty;
         public string Alias { get; set; } = string.Empty;
         public string Country { get; set; } = string.Empty;
-        public ICollection<Driver> Drivers { get; set; }
+        public ICollection<Driver>? Drivers { get; set; }
     }
 }
